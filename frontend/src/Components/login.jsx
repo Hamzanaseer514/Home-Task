852e337c25065f58fe5975e426b2e6f85df3f75c
Ameer Hamza <hamzanaseer496@gmail.com>
2024-11-10 18:12:09 +0500

    Update login.jsx

--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/auth/login', {
+      const response = await fetch('https://se-lab-backend-pe25.onrender.com/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
