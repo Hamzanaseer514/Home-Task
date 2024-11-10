1fd329bf551b72fd4249e1ee46082db3a957c7a9
Ameer Hamza <hamzanaseer496@gmail.com>
2024-11-10 18:11:48 +0500

    Update Signup.jsx

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/auth/signup', {
+      const response = await fetch('https://se-lab-backend-pe25.onrender.com/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
